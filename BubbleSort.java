52e94bf55da9dba1e7f281aa27fd754fc8902e7f
Akkun716 <T1M34git_HUB>
2019-10-09 09:01:30 -0700

    Created more efficient bubblesort

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,16 +1,19 @@
 public class BubbleSort implements SortingAlgorithm
 {
-    public void sort(int [] a)
+    public void sort(int[] a)
     {
+        boolean sorted = false;
         //Repeats the moving of respective largest num to respective places
-        for(int pass = 0; pass < a.length; pass++)
+        while(!sorted)
         {
+            sorted = true;
             //Moves through array one by one
             for(int i = 0; i < a.length - 1; i++)
             {
                 //If the current number is bigger than the next
                 if(a[i] > a[i + 1])
                 {
+                    sorted = false;
                     //Switch their placements
                     int temp = a[i];
                     a[i] = a[i + 1];
